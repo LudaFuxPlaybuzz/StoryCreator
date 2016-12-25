78df6b51509e07113ab17084044b6918b9dea629
Luda Fux <fuxlud@gmail.com>
2016-12-25 14:25:55 +0200

    scroll

--- StoryCreator/Controller/Particle/ParticleTableViewCell.swift
+++ StoryCreator/Controller/Particle/ParticleTableViewCell.swift
@@ -18,7 +18,10 @@ class ParticleTableViewCell: UITableViewCell
     override func awakeFromNib()
     {
         super.awakeFromNib()
-        
+
+        cardBackgroundBorder.path = UIBezierPath(rect: cardBackground.bounds).cgPath
+        cardBackground.layer.addSublayer(cardBackgroundBorder)
+
         cardBackgroundBorder.strokeColor = UIColor.init(colorLiteralRed: 0.5, green: 0.5, blue: 0.5, alpha: 0.25).cgColor
         cardBackgroundBorder.borderWidth = 3
         cardBackgroundBorder.fillColor = nil
@@ -29,8 +32,6 @@ class ParticleTableViewCell: UITableViewCell
     {
         super.layoutSubviews()
         
-        cardBackgroundBorder.path = UIBezierPath(rect: cardBackground.bounds).cgPath
-        cardBackground.layer.addSublayer(cardBackgroundBorder)
     }
     
     override func setSelected(_ selected: Bool, animated: Bool)
