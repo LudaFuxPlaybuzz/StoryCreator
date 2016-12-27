3d9563e40bc3b89bac7aa5585dbed1f0f7a826b2
Luda Fux <fuxlud@gmail.com>
2016-12-27 17:50:44 +0200

    text fields

--- StoryCreator/ViewController.swift
+++ StoryCreator/ViewController.swift
@@ -14,11 +14,11 @@ class ViewController: UIViewController, UITableViewDataSource, NewParticlesColle
 {
     @IBOutlet weak var createButton: UIButton!
     @IBOutlet weak var particlesTable: UITableView!
-    @IBOutlet weak var descriptionBackground: UIView!
     @IBOutlet weak var particlesTableHeight: NSLayoutConstraint!
     @IBOutlet weak var containerHeightConstraint: NSLayoutConstraint!
     @IBOutlet weak var coverImage: UIImageView!
     @IBOutlet weak var titleTextField: AnimatedTextInput!
+    @IBOutlet weak var descriptionTextField: AnimatedTextInput!
     
     let descriptionBackgroundBorder = CAShapeLayer()
     var newParticles = [NewParticleObject]()
@@ -30,20 +30,24 @@ class ViewController: UIViewController, UITableViewDataSource, NewParticlesColle
         createButton.layer.masksToBounds = true
         createButton.layer.cornerRadius = 6
      
-        descriptionBackgroundBorder.path = UIBezierPath(rect: descriptionBackground.bounds).cgPath
-        descriptionBackground.layer.addSublayer(descriptionBackgroundBorder)
+//        descriptionBackgroundBorder.path = UIBezierPath(rect: descriptionTextField.bounds).cgPath
+//        descriptionTextField.layer.addSublayer(descriptionBackgroundBorder)
         particlesTable.setEditing(true, animated: true)
         
         titleTextField.style = TitleInputStyle() as AnimatedTextInputStyle
         titleTextField.backgroundColor = UIColor.clear
         titleTextField.placeHolderText = "Title"
+        
+        descriptionTextField.style = DescriptionInputStyle() as AnimatedTextInputStyle
+        descriptionTextField.placeHolderText = "Description"
+        descriptionTextField.type = .multiline
     }
     
     override func viewDidLayoutSubviews()
     {
-        descriptionBackgroundBorder.strokeColor = UIColor.init(colorLiteralRed: 0.5, green: 0.5, blue: 0.5, alpha: 0.25).cgColor
-        descriptionBackgroundBorder.fillColor = nil
-        descriptionBackgroundBorder.lineDashPattern = [12, 8]
+//        descriptionBackgroundBorder.strokeColor = UIColor.init(colorLiteralRed: 0.5, green: 0.5, blue: 0.5, alpha: 0.25).cgColor
+//        descriptionBackgroundBorder.fillColor = nil
+//        descriptionBackgroundBorder.lineDashPattern = [12, 8]
         
         particlesTableHeight.constant = particlesTable.contentSize.height
         
@@ -145,7 +149,7 @@ class ViewController: UIViewController, UITableViewDataSource, NewParticlesColle
         let counterLabelFont: UIFont? = UIFont.systemFont(ofSize: 9)
         let leftMargin: CGFloat = 0
         let topMargin: CGFloat = 20
-        let rightMargin: CGFloat = 0
+        let rightMargin: CGFloat = 15
         let bottomMargin: CGFloat = 10
         let yHintPositionOffset: CGFloat = 7
         let yPlaceholderPositionOffset: CGFloat = 7
@@ -154,18 +158,18 @@ class ViewController: UIViewController, UITableViewDataSource, NewParticlesColle
     
     struct DescriptionInputStyle: AnimatedTextInputStyle {
         
-        let activeColor = UIColor.black
-        let inactiveColor = UIColor.gray//.withAlphaComponent(0.3)
+        let activeColor = UIColor.gray.withAlphaComponent(0.3)
+        let inactiveColor = UIColor.gray.withAlphaComponent(0.3)
         let lineInactiveColor = UIColor.gray.withAlphaComponent(0.3)
         let errorColor = UIColor.red
-        let textInputFont = UIFont.systemFont(ofSize: 25)
-        let textInputFontColor = UIColor.white
+        let textInputFont = UIFont.systemFont(ofSize: 20)
+        let textInputFontColor = UIColor.black
         let placeholderMinFontSize: CGFloat = 9
         let counterLabelFont: UIFont? = UIFont.systemFont(ofSize: 9)
-        let leftMargin: CGFloat = 0
+        let leftMargin: CGFloat = 15
         let topMargin: CGFloat = 20
-        let rightMargin: CGFloat = 0
-        let bottomMargin: CGFloat = 10
+        let rightMargin: CGFloat = 10
+        let bottomMargin: CGFloat = 15
         let yHintPositionOffset: CGFloat = 7
         let yPlaceholderPositionOffset: CGFloat = 7
         
